b9c8e569cd4e5544e84b4a07ac1c2019d73cbec1
Alexey Baranov <Amaentes@icloud.com>
2023-01-29 17:15:25 +0300

    number9

--- number9/Program.cs
+++ number9/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+
+Random rnd = new Random();
+int value = rnd.Next(10, 99);
+Console.WriteLine(value); 
+int a = 0;
+int b = 0;
+a = value / 10; //1 цифра
+b = value % 10; //2 цифра
+if(a > b)
+{
+  Console.WriteLine("Наибольшая цифра числа {0}", a);
+}
+else
+{
+  Console.WriteLine("Наибольшая цифра числа {0}", b);    
+}
